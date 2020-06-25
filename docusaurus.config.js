8a409a2d02eb20208347257fbf906e7a032790a4
flytreeleft <flytreeleft@crazydan.org>
2020-06-26 00:58:12 +0800

    Add source code for crazydan.org

---
+++ docusaurus.config.js
@@ -0,0 +1,105 @@
+module.exports = {
+  title: 'Crazydan Studio',
+  tagline: 'Make life easier and happier.',
+  url: 'https://crazydan.org',
+  baseUrl: '/',
+  favicon: 'img/logo.png',
+  organizationName: 'crazydan-studio',
+  projectName: 'crazydan.org',
+  themeConfig: {
+    navbar: {
+      title: 'Crazydan Studio',
+      logo: {
+        alt: 'Crazydan Studio',
+        src: 'img/logo.png',
+      },
+      links: [
+        // {
+        //   to: 'docs/',
+        //   activeBasePath: 'docs',
+        //   label: 'Docs',
+        //   position: 'left',
+        // },
+        // {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://git.crazydan.org/explore/repos',
+          label: 'Open Source',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        // {
+        //   title: 'Docs',
+        //   items: [
+        //     // {
+        //     //   label: 'Style Guide',
+        //     //   to: 'docs/',
+        //     // },
+        //     // {
+        //     //   label: 'Second Doc',
+        //     //   to: 'docs/doc2/',
+        //     // },
+        //   ],
+        // },
+        // {
+        //   title: 'Community',
+        //   items: [
+        //     // {
+        //     //   label: 'Stack Overflow',
+        //     //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //     // },
+        //     // {
+        //     //   label: 'Discord',
+        //     //   href: 'https://discordapp.com/invite/docusaurus',
+        //     // },
+        //     // {
+        //     //   label: 'Twitter',
+        //     //   href: 'https://twitter.com/docusaurus',
+        //     // },
+        //   ],
+        // },
+        // {
+        //   title: 'More',
+        //   items: [
+        //     // {
+        //     //   label: 'Blog',
+        //     //   to: 'blog',
+        //     // },
+        //     // {
+        //     //   label: 'GitHub',
+        //     //   href: 'https://github.com/facebook/docusaurus',
+        //     // },
+        //   ],
+        // },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Crazydan Studio. Built with <a href="https://v2.docusaurus.io/">Docusaurus 2</a>.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        // docs: {
+        //   // It is recommended to set document id as docs home page (`docs/` path).
+        //   homePageId: 'doc1',
+        //   sidebarPath: require.resolve('./sidebars.js'),
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/edit/master/website/',
+        // },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: <>工匠坚守</>,
+    imageUrl: 'img/artisan-sprit.jpg',
+    description: (
+      <>
+        以工匠持之以恒的态度和精益求精的精神，创作和打磨精良的软件产品。
+      </>
+    ),
+  },
+  {
+    title: <>大道至简</>,
+    imageUrl: 'img/kiss.jpg',
+    description: (
+      <>
+        始终探索最简的解决方案，以简单且资源消耗最少的方式解决看似复杂的问题。
+      </>
+    ),
+  },
+  {
+    title: <>凝望星空</>,
+    imageUrl: 'img/stars-sky.jpg',
+    description: (
+      <>
+        遥望浩瀚宇宙，畅想遥远未来，力争打造出能够跨越时空，影响世代的作品。
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Welcome to ${siteConfig.title}`}
+      description={`${siteConfig.title}: ${siteConfig.tagline}`}>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            {/* <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link> */}
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container text--center">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
