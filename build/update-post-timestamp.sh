5601a8076f25a43b79554c15725396b658c3a573
flytreeleft <flytreeleft@crazydan.org>
2020-10-25 16:57:21 +0800

    Update the blog's timestamp based on the git commits before building the posts

---
+++ build/update-post-timestamp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+post_dir=blog
+
+# https://github.com/theme-next/hexo-theme-next/issues/893#issuecomment-498080459
+# %ai: 2018-02-25 16:53:50 +0800
+# %ad: Wed Oct 2 00:00:43 2019 +0800
+# %ct: 1570612408
+git ls-files -z "$post_dir" \
+    | while read -d '' path; do \
+        touch -d "$(git log -1 --format="@%ct" -- "$path")" "$path"; \
+      done
