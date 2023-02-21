27de55021e6c5a162d807dd922c66d5b82913e59
flytreeleft <flytreeleft@crazydan.org>
2023-02-21 15:26:42 +0800

    Component: Project页按分类展示各项目

--- src/components/Projects/List.js
+++ src/components/Projects/List.js
@@ -6,11 +6,42 @@ import {
   ThemeClassNames
 } from '@docusaurus/theme-common';
 import MDXContent from '@theme/MDXContent';
+import BlogSidebar from '@theme/BlogSidebar';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { BlogPostProvider } from '@docusaurus/theme-common/internal';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
+
+function sortProjectByCategory(projects) {
+  const categoryProjects = {};
+  const addProjectByCategory = (category, project) => {
+    categoryProjects[category] = [
+      ...(categoryProjects[category] || []),
+      project
+    ];
+  };
+
+  projects.forEach((project) => {
+    const categories = project.content.metadata.frontMatter.categories || [];
+
+    if (categories.length == 0) {
+      addProjectByCategory('default', project);
+    } else {
+      categories.forEach((category) => {
+        addProjectByCategory(category, project);
+      });
+    }
+  });
+
+  for (const category in categoryProjects) {
+    categoryProjects[category] = categoryProjects[category].sort((a, b) =>
+      a.content.metadata.title.localeCompare(b.content.metadata.title)
+    );
+  }
+
+  return categoryProjects;
+}
 
 function Project({ children, frontMatter, title }) {
   const imageUrl = useBaseUrl(frontMatter.image);
@@ -56,11 +87,46 @@ function Project({ children, frontMatter, title }) {
   );
 }
 
+function ProjectList({ category, projects }) {
+  return (
+    <div>
+      <h2 id={category} className={clsx(styles.projectCategory)}>
+        <div className={clsx(styles.projectCategorySeparator)}></div>
+        <Link className={clsx('hash-link')} to={'#' + category} />
+        {category}
+        <div style={{ width: '24px' }}></div>
+        <div className={clsx(styles.projectCategorySeparator)}></div>
+      </h2>
+      <div className={clsx(styles.projectList)}>
+        {projects.map(({ content: BlogPostContent }, idx) => (
+          <BlogPostProvider
+            key={category + '-p-' + BlogPostContent.metadata.permalink}
+            content={BlogPostContent}
+          >
+            <Project key={category + '-p-' + idx} {...BlogPostContent.metadata}>
+              <BlogPostContent />
+            </Project>
+          </BlogPostProvider>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 function Component({ items, metadata }) {
-  console.log(items, arguments);
   const context = useDocusaurusContext();
   const { blogTitle } = metadata;
   const { siteConfig = {} } = context;
+
+  const categoryProjects = sortProjectByCategory(items);
+  const categoryProjectsKeys = Object.keys(categoryProjects).sort();
+  const sidebar = {
+    title: 'Project categories',
+    items: categoryProjectsKeys.map((key) => ({
+      title: key,
+      permalink: '#' + key
+    }))
+  };
 
   return (
     <HtmlClassNameProvider
@@ -75,17 +141,14 @@ function Component({ items, metadata }) {
       >
         <div className="container margin-vert--lg">
           <div className="row">
-            <aside className={clsx('col col--3')}></aside>
+            <BlogSidebar sidebar={sidebar} />
             <main className={clsx('col col--7')}>
-              {items.map(({ content: BlogPostContent }, idx) => (
-                <BlogPostProvider
-                  key={BlogPostContent.metadata.permalink}
-                  content={BlogPostContent}
-                >
-                  <Project key={idx} {...BlogPostContent.metadata}>
-                    <BlogPostContent />
-                  </Project>
-                </BlogPostProvider>
+              {categoryProjectsKeys.map((category) => (
+                <ProjectList
+                  key={category}
+                  category={category}
+                  projects={categoryProjects[category]}
+                />
               ))}
             </main>
           </div>
