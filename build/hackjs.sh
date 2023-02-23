2606f3099f33281c837ce31590b7718d288edbae
flytreeleft <flytreeleft@crazydan.org>
2023-02-23 13:05:12 +0800

    Build: hackjs.sh 增加调试输出

--- build/hackjs.sh
+++ build/hackjs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 _DIR_="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-set -e
+set -xe
 
 NODE_MODULES_PATH="${_DIR_}/.."
 
 cp -f "${_DIR_}/hackjs/docusaurusContext.js" \
-    "${NODE_MODULES_PATH}/node_modules/@docusaurus/core/lib/client/docusaurusContext.js"
+    "${NODE_MODULES_PATH}/node_modules/@docusaurus/core/lib/client/docusaurusContext.js" \
+&& cat "${NODE_MODULES_PATH}/node_modules/@docusaurus/core/lib/client/docusaurusContext.js"
+
 cp -f "${_DIR_}/hackjs/LocaleDropdownNavbarItem.js" \
-    "${NODE_MODULES_PATH}/node_modules/@docusaurus/theme-classic/lib/theme/NavbarItem/LocaleDropdownNavbarItem/index.js"
+    "${NODE_MODULES_PATH}/node_modules/@docusaurus/theme-classic/lib/theme/NavbarItem/LocaleDropdownNavbarItem/index.js" \
+&& cat "${NODE_MODULES_PATH}/node_modules/@docusaurus/theme-classic/lib/theme/NavbarItem/LocaleDropdownNavbarItem/index.js"
