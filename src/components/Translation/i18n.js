5bd9c689ccdec63f5fe0ff680d4d9b4b430fe67a
flytreeleft <flytreeleft@crazydan.org>
2023-02-24 22:48:27 +0800

    Components: 新增Translation组件，用于翻译文章段落

---
+++ src/components/Translation/i18n.js
@@ -0,0 +1,15 @@
+import { i18n, arg } from '@site/src/components/I18n';
+
+export default i18n()
+  //
+  .trans(['中文'])
+  .en('Chinese')
+  //
+  .trans(['英文'])
+  .en('English')
+  //
+  .trans([arg(), '原文'])
+  .en((name) => `Original (${name})`)
+  .zh((name) => `原文 (${name})`)
+  //
+  .done();
---
+++ src/components/Translation/index.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import clsx from 'clsx';
+
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+import { currentLocale } from '@site/src/components/I18n';
+
+import i18n from './i18n';
+import styles from './styles.module.css';
+
+const langNames = {
+  zh: '中文',
+  en: '英文'
+};
+
+export function Text({ children }) {
+  return <div className={clsx(styles.translationText)}>{children}</div>;
+}
+
+export function Translation({ children, titled }) {
+  const locale = currentLocale();
+
+  const items = Array.isArray(children) ? children : [children];
+  const sourceItem = items.filter((item) => item.props.source)[0];
+  const otherItems = items.filter((item) => !item.props.source);
+  const sortedItems = sourceItem ? [sourceItem].concat(otherItems) : otherItems;
+
+  const Comp = titled ? TranslationTitle : TranslationParagraph;
+
+  return <Comp locale={locale} source={sourceItem} items={sortedItems} />;
+}
+
+function TranslationParagraph({ locale, items }) {
+  return (
+    <div className={clsx(styles.translationContainer)}>
+      <Tabs className={clsx(styles.translation)} /*groupId="translation"*/>
+        {items.map(({ props, type: TextComp }, idx) => (
+          <TabItem
+            key={idx}
+            value={props.lang}
+            label={i18n(
+              i18n(langNames[props.lang] || props.lang),
+              props.source ? '原文' : ''
+            )}
+            default={
+              items.length === 1
+                ? true
+                : locale
+                ? locale === props.lang
+                : idx === 0
+            }
+          >
+            <TextComp {...props} />
+          </TabItem>
+        ))}
+      </Tabs>
+    </div>
+  );
+}
+
+function TranslationTitle({ locale, source, items }) {
+  const titleId = (source || items[0]).props.children.props.id;
+
+  if (typeof window !== 'undefined') {
+    items.forEach((item) => {
+      const itemId = item.props.children.props.id;
+      if (itemId === titleId) {
+        return;
+      }
+
+      const style = document.createElement('style');
+      style.innerText = `.table-of-contents__link[href="#${itemId}"] { display: none; }`;
+      document.body.appendChild(style);
+    });
+  }
+
+  return (
+    <div
+      className={clsx(
+        styles.translationContainer,
+        styles.translationTitleContainer
+      )}
+      id={titleId}
+    >
+      <Tabs className={clsx(styles.translation)} /*groupId="translation"*/>
+        {items.map(({ props }, idx) => (
+          <TabItem
+            key={idx}
+            value={props.lang}
+            label={i18n(
+              i18n(langNames[props.lang] || props.lang),
+              props.source ? '原文' : ''
+            )}
+            default={
+              items.length === 1
+                ? true
+                : locale
+                ? locale === props.lang
+                : idx === 0
+            }
+          >
+            {[]
+              .concat(props.children)
+              .map(({ props, type: TitleComp }, idx) => (
+                <TitleComp key={idx} {...props} id="">
+                  {props.children}
+                  <a href={`#${titleId}`} className="hash-link"></a>
+                </TitleComp>
+              ))}
+          </TabItem>
+        ))}
+      </Tabs>
+    </div>
+  );
+}
